91c950362f8bbc418b6dcb34df6cf0465944066e
TalyLisboa <talitalisboapiccolo@gmail.com>
2025-03-12 15:22:39 -0300

    Módulo 1 e criando seu primeiro programa Hello World!

---
+++ modulo-1-primeiros-passos/primeiro-programa/hello-world.js
@@ -0,0 +1,24 @@
+// Meu primeiro programa JavaScript
+
+// 1. Mostrando uma mensagem no console
+console.log("Olá, Mundo!");
+
+// 2. Armazenando a mensagem em uma variável
+let mensagem = "Bem-vindo ao JavaScript Descomplicado!";
+console.log(mensagem);
+
+// 3. Combinando texto (concatenação)
+let nome = "Estudante";
+console.log("Olá, " + nome + "! Que bom ter você aqui!");
+
+// 4. Template strings (forma moderna)
+console.log(`${nome}, você acabou de executar seu primeiro código JavaScript!`);
+
+/*
+  Para executar este código:
+  - No navegador: abra o console (F12) e cole o código
+  - Com Node.js: abra o terminal e digite: node hello-world.js
+*/
+
+// 5. Interagindo com uma página web (execute isso no console do navegador)
+// alert("Olá, Mundo! Esta é uma caixa de diálogo.");
